f7c82e45c0ad3932f07201e93f960a2aa52a7b60
“Zeeshan <zeeshanraza252627@gmail.com>
2021-06-20 20:31:00 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Col, Row, Input, Button } from 'antd';
+import pic from './assets/pic.png';
+import logo from './assets/logo.png';
+import { ArrowLeftOutlined, PushpinOutlined, WechatOutlined } from '@ant-design/icons';
+import { style } from './style'
 
 function App() {
+  const {
+    firstCol,
+    veriCode,
+    otp,
+    otpInput,
+    submitButton,
+    divStyle,
+    arrowLeft,
+    spanStyle,
+    imgCol,
+    chatColStyle,
+    weChatCol,
+    weChatIcon,
+    commentButtons
+  } = style
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Row>
+      <Col span={1}></Col>
+      <Col span={6} style={firstCol}>
+        <img src={logo} width="100px" alt="" srcset="" />
+        <h3 style={veriCode}>Verification Code</h3>
+        <p>Please type the verification code sent to your email or mobile number <b>+917899878778</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <span style={otp}>Enter OTP</span>
+        <Input placeholder="Enter OTP" style={otpInput} />
+        <Button type="primary"
+          style={submitButton}>
+          Submit
+        </Button>
+        <div style={divStyle}>
+          <ArrowLeftOutlined style={arrowLeft} />
+          <span style={spanStyle}>Change Details</span>
+        </div>
+      </Col>
+      <Col span={10} style={imgCol}>
+        <img src={pic} width="100%" alt="" srcset="" />
+      </Col>
+      <Col span={5} style={chatColStyle}>
+        <Input placeholder="Make a comment" suffix={<PushpinOutlined />} />
+        <Row justify="end" style={commentButtons}>
+          <Col span={8}>
+            <Button>Cancel</Button>
+          </Col>
+          <Col span={4}>
+            <Button>Submit</Button>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+        <hr />
+        <Row justify="center" align="middle" >
+          <Col span={18} style={weChatCol}>
+            <WechatOutlined style={weChatIcon} />
+            <h3>Got something to say?</h3>
+            <h5>
+              Comment to start a discussion, make a note, or annotate the artboard.
+            </h5>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,15 @@
+export const style = {
+    firstCol: { marginTop: '100px' },
+    veriCode: { marginTop: '50px', fontWeight: 'bolder' },
+    otp: { fontSize: '0.7rem', fontWeight: 'bolder' },
+    otpInput: { width: "90%", margin: '10px 0px 20px 0px' },
+    submitButton: { backgroundColor: 'orange', border: 'none', color: 'black' },
+    divStyle: { margin: '20px 0px' },
+    arrowLeft: { fontSize: "12px", margin: '0px 8px 0px 0px' },
+    spanStyle: { fontWeight: 'bold', margin: '10px 0px 20px 0px', fontSize: '0.7rem' },
+    imgCol: { marginTop: '100px' },
+    chatColStyle: { margin: '100px 20px' },
+    weChatCol: { textAlign: 'center', marginTop: '25vh' },
+    weChatIcon: { fontSize: '45px' },
+    commentButtons: { marginTop: '20px' }
+}
